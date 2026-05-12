3bf31a0dec81e743e99087db04ce361f85b746ec
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2026-05-12 23:21:00 +0800

    do brute force the recursion way

---
+++ javascript/fibonacci/brute-force-recursion.js
@@ -0,0 +1,19 @@
+fib = (n) => {
+	if (n <= 0) {
+		return 0
+	}
+
+	if (n <= 2) {
+		return 1
+	}
+
+	return fib(n - 2) + fib(n - 1)
+}
+
+
+console.log(fib(0)) //0
+console.log(fib(1)) //1
+console.log(fib(2)) //1
+console.log(fib(6)) //8
+console.log(fib(10)) //55
+
